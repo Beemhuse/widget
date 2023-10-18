e839d8064cf21be025d6a113e43a7daa395755ab
Beemhuse <brightawah94@gmail.com>
2023-10-18 20:57:14 +0100

    fix error on send

--- chat-widget.js
+++ chat-widget.js
@@ -7,6 +7,7 @@
         socket: null,
         init: function() {
             const socketUrl = `wss://api.andromedia.cc/ws/chat/${conversationId}/`;
+            let isSending = false; // Flag to prevent multiple message sends
 
             const chatWidgetContainer = document.createElement('div');
             chatWidgetContainer.id = 'chatWidgetContainer';
@@ -15,12 +16,18 @@
 
             const chatContainer = document.createElement('div');
             chatContainer.style.position = 'fixed';
+            chatContainer.style.display = 'flex';
+            chatContainer.style.flexDirection = 'column';
+            chatContainer.style.justifyContent = 'end';
             chatContainer.style.bottom = '20px';
             chatContainer.style.right = '20px';
             chatContainer.style.width = '300px'; // Set your desired width
             chatContainer.style.height = '400px'; // Set your desired height
             chatContainer.style.border = '1px solid #ccc';
-            chatContainer.style.background = '#fff';
+            chatContainer.style.background = '#ff0000';
+            chatContainer.style.borderTopRightRadius = '20px';
+            chatContainer.style.borderTopLeftRadius = '20px';
+
             chatContainer.style.overflow = 'auto';
             chatContainer.classList.add('chat-container');
 
@@ -30,11 +37,7 @@
             inputElement.classList.add('message-input');
             chatContainer.appendChild(inputElement); // Add input element to chat container
 
-            const sendButton = document.createElement('button');
-            sendButton.innerText = 'Send';
-            sendButton.addEventListener('click', sendMessage);
-            chatContainer.appendChild(sendButton); // Add send button to chat container
-
+           
             chatWidgetContainer.appendChild(chatContainer);
             this.socket = new WebSocket(socketUrl);
 
@@ -45,23 +48,33 @@
                 chatContainer.appendChild(messageDiv);
             }
 
-            function sendMessage() {
-                const messageInput = inputElement.value.trim();
-                if (messageInput) {
-                    const message = {
-                        type: 'message',
-                        message: messageInput,
-                        conversation: conversationId,
-                        sender_id: userId,
-                        receiver_id: customerId,
-                        sender_type: 'customer',
-                        receiver_type: 'user',
-                    };
-                    this.socket.send(JSON.stringify(message));
-                    addChatMessage(messageInput, 'User');
-                    inputElement.value = '';
+            const sendMessage = () => {
+                if (this.socket.readyState === WebSocket.OPEN) {
+                    const messageInput = inputElement.value.trim();
+                    if (messageInput) {
+                        const message = {
+                            type: 'message',
+                            message: messageInput,
+                            conversation: conversationId,
+                            sender_id: userId,
+                            receiver_id: customerId,
+                            sender_type: 'customer',
+                            receiver_type: 'user',
+                        };
+                        this.socket.send(JSON.stringify(message));
+                        addChatMessage(messageInput, 'User');
+                        inputElement.value = '';
+                    }
+                } else {
+                    console.error('WebSocket connection is closed. Unable to send message.');
+                
                 }
-            }
+            };
+    
+    const sendButton = document.createElement('button');
+    sendButton.innerText = 'Send';
+    sendButton.addEventListener('click', sendMessage);
+    chatContainer.appendChild(sendButton); // Add send button to chat container
 
             this.socket.addEventListener('message', (event) => {
                 const message = JSON.parse(event.data);
